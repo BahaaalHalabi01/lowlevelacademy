183d6113e4dbf6ae6013577c9ebeec0f906df06f
bahaa <bahaa.alhalabi01@mail.ru>
2025-01-02 23:58:41 +0300

    union wtf

---
+++ union.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+union myunion_u {
+  int x;
+  char c;
+  short s;
+};
+
+int main() {
+
+  union myunion_u u;
+
+  u.x = 0x41424344;
+
+  printf("%hx %hhx\n", u.s, u.c);
+
+  return 0;
+}
