a5a164a254927dd50edefb178d154df5b60e5578
bahaa <bahaa.alhalabi01@mail.ru>
2025-01-06 00:41:07 +0300

    read from argv

--- file-input.c
+++ file-input.c
@@ -8,9 +8,14 @@ struct database_header_t {
   unsigned int filelength;
 };
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  int fd = open("./local.db", O_RDONLY);
+  if (argc != 2) {
+    printf("Incorrect usage of program. Use %s <file_name> \n", argv[0]);
+    return -1;
+  }
+
+  int fd = open(argv[1], O_RDONLY);
   if (fd == -1) {
     perror("open");
     return -1;
@@ -23,9 +28,9 @@ int main() {
     return -1;
   }
 
-printf("Database Version %d\n", header.version);
-printf("Database employees %d\n", header.employees);
-printf("Database filelength %d\n", header.filelength);
+  printf("Database Version %d\n", header.version);
+  printf("Database employees %d\n", header.employees);
+  printf("Database filelength %d\n", header.filelength);
 
   return 0;
 }
