8408baef496064e60fce20f37f77089c58dbe073
bahaa <bahaa.alhalabi01@mail.ru>
2025-01-08 02:38:07 +0300

    print usage

--- src/main.c
+++ src/main.c
@@ -4,10 +4,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "../include/file.h"
+#include "../include/parse.h"
+
+void print_usage(char *argv[]) {
+
+
+  printf("Usage: %s -n -f <datbase path>\n",argv[0]);
+  printf("\t -n - create a new database file\n");
+  printf("\t -f - (required) path to a database file\n");
+  return;
+
+}
+
 int main(int argc, char *argv[]) {
 
   int c;
-
   char *db_path = NULL;
   bool new_file = false;
 
@@ -30,8 +42,14 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  printf("Create a new file %d\n",new_file);
-  printf("With db path  %s\n",db_path);
+  if (db_path == NULL) {
+    printf("Database path is a required argument\n");
+    print_usage(argv);
+    return 0;
+  }
+
+  printf("Create a new file %d\n", new_file);
+  printf("With db path  %s\n", db_path);
 
   return 0;
 }
