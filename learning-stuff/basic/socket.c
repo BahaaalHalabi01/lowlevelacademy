6136b704a90513838fe356a56927b6d525aae37e
bahaa <bahaa.alhalabi01@mail.ru>
2025-01-12 00:13:08 +0300

    netwoork start -socket

---
+++ learning-stuff/basic/socket.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (fd == -1) {
+    perror("socket");
+    return -1;
+  }
+
+  printf("%d\n",fd);
+
+  return 0;
+
+}
